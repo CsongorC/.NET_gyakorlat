7e58cd4db75e315f5aa4969acef3a8f181d785ad
Csongor Csegezi <csegezicsongor@yahoo.com>
2022-02-21 10:13:52 +0200

    labor 1

---
+++ cliens_app/cliens_app/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cliens_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            people.Add(new Person { firstName = "John", lastName = "Doe" });
+            people.Add(new Person { firstName = "Joe", lastName = "Smith" });
+            people.Add(new Person { firstName = "Sue", lastName = "Storm" });
+
+            foreach(Person person in people)
+            {
+                person.setFullName();
+            }
+            NameComboBx.ItemsSource = people;
+            
+
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameTxtBx.Text} {lastNameTxtBx.Text}");
+        }
+    }
+}
---
+++ cliens_app/cliens_app/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cliens_app
+{
+    public class Person
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+
+        public string fullName { get; set; }
+
+        public void setFullName()
+        {
+            this.fullName = this.lastName +" "+ this.firstName;
+        }
+
+    }
+}
